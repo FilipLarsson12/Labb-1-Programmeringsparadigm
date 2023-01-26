4ec11218b68df6de996af4460ed64dd57baf9a29
Filip Larsson <hockeyfilip12@gmail.com>
2023-01-26 18:12:29 +0100

    Uppgift 2 med kommentarer

--- labb1.hs
+++ labb1.hs
@@ -17,3 +17,31 @@ karpsravor (x:xs)
     | otherwise = x : karpsravor (tail (tail xs))
 
 
+-- Uppgift 3
+
+-- Function that counts the number of words in a String by checking for character 
+-- Followed by a space.
+
+countWords :: String -> Double
+countWords [] = 0
+countWords (x:xs)
+        | (isAlpha x == True && xs == []) = 1 + countWords xs
+        | (isAlpha x == True) && ( isAlpha (head(xs)) == False) = 1 + countWords xs
+        | otherwise = countWords xs
+
+-- Function that counts the number of characters in a String by checking if 
+-- every value is True or False when passed to the isAlpha function. 
+
+
+countCharacters :: String -> Double
+countCharacters [] = 0
+countCharacters (x:xs)
+        | (isAlpha x == True) = 1 + countCharacters xs
+        | otherwise = countCharacters xs
+
+
+-- Function that checks the average length of a word in a sentence by dividing
+-- number of characters by the number of words.
+medellangd :: String -> Double
+medellangd [] = 0
+medellangd x = countCharacters x/countWords x
