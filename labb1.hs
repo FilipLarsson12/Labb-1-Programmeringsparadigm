e9f2c58e2d9e16ef3d417426e62e513a4d1287df
Filip Larsson <hockeyfilip12@gmail.com>
2023-01-26 13:59:45 +0100

    Uppgift 2

---
+++ labb1.hs
@@ -0,0 +1,17 @@
+import Data.Char
+import Data.List
+
+rovarsprak :: String -> String
+rovarsprak [] = []
+rovarsprak (x:xs)
+    | elem x "aeiouy" = x : (rovarsprak xs)
+    | otherwise = x : 'o' : x : (rovarsprak xs)
+
+
+karpsravor :: String -> String
+karpsravor [] = []
+karpsravor (x:xs)
+    | elem x ("aeiouy") = x : karpsravor xs
+    | otherwise = x : karpsravor (tail (tail xs))
+
+
